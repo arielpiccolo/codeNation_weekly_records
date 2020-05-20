a94edc8a89672e4dc78c1a0a4f048f74ea754313
arielpiccolo <arielpiccolo@gmail.com>
2020-05-20 23:05:00 +0100

    chanllenge5day3 finished

--- classes/week1/day3/challenge5day3.js
+++ classes/week1/day3/challenge5day3.js
@@ -2,14 +2,17 @@ let timeOfTheDay = (Math.round(Math.random() * 24));
 
 const person = 'John';
 const workingHours = [9, 10, 11, 12, 13 , 14, 15, 16, 17,];
-const offWork = [18, 19, 20, 21, 22, 23, 24, 1, 2, 3, 4, 5, 6,7, 8]
-let placeOfWork = 'Manchester';
+const offWork = [18, 19, 20, 21, 22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8];
+let placeOfWork = 'CodeNation! Manchester';
 let homeTown = 'Leeds';
-const workingWeek = ['Monday', 'Tuesday', 'Wedesday', 'Thursday', 'Friday']
-const weekEnd = ['Satuday', 'Sunday'];
-let day = days[Math.floor(Math.random() * items.lenght)];
+const week = 'Monday, Tuesday, Wedesday, Thursday, Friday, Saturday, Sunday';
+let todayIs = week[Math.floor(Math.random()*week.length)];
+let weekEnd = todayIs.search("Saturday, Sunday");
 
-if (timeOfTheDay in offWork || weenEnd in day){
-  console.log(`${person} is at home in ${homeTown}`);
+
+if (timeOfTheDay in offWork || weekEnd == true ){
+  console.log(`\n${person} is at home in ${homeTown}`);
+} else {
+  console.log(`\n${person} is at work at ${placeOfWork}`)
 }
 
