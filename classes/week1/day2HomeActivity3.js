a881e9b63f6cb6e901ab4b4e368bfd0be12b8f8a
arielpiccolo <arielpiccolo@gmail.com>
2020-05-20 09:06:00 +0100

    activity 3 submited

--- classes/week1/day2HomeActivity3.js
+++ classes/week1/day2HomeActivity3.js
@@ -1,3 +1,12 @@
-let todaysDate = Date();
-const myBirthday = new Date(2020,09,13);
- console.log()
+// first we set our birthday variable
+let birthday = '2020-09-13';
+// then we set today's day
+let today = '2020-05-20';
+/* then we substract birthday and today variable and store the output in a new variable.(js will store the output in seconds) */ 
+const timeDiff  = (new Date(birthday)) - (new Date(today)); 
+/* we then create a new variable to store the total amount of seconds in 24hrs (the last *1000 bit is just to take care of milliseconds) */
+const secsIn24Hours = 24 * 60 * 60 * 1000;
+/* next we divide timeDiff variable output and millisecsIn24Hrs variable */
+let days = timeDiff / secsIn24Hours;
+
+console.log(days);
