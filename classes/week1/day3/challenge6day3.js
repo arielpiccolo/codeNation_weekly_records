e7d81099f2e7afd5dbcd9d9732520e50a2408e65
arielpiccolo <arielpiccolo@gmail.com>
2020-05-21 10:29:44 +0100

    challenge6day3Finished

---
+++ classes/week1/day3/challenge6day3.js
@@ -0,0 +1,7 @@
+// create a string variable
+let findIndex = "jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi";
+
+// index it to find last character and log it out
+let index = findIndex.charAt(findIndex.length -1)
+
+console.log(index)
