8cb682089e56d9c00c488cb1ecc318fe924a3e5c
arielpiccolo <arielpiccolo@gmail.com>
2020-05-21 22:38:25 +0100

    arrayActivity1 websites array

---
+++ classes/week1/day4/arrayActivity1(3favoritesWebsites).js
@@ -0,0 +1,21 @@
+//lets create and array with 3 items in it
+let favoritesWebsites = ['google.com', 'yahoo.com', 'msn.com']
+
+//lets add another 2 item into the array using .push method. Push will add anohter item to the last possition of the array
+favoritesWebsites.push('bbc.com')
+favoritesWebsites.push('mozilla.org')
+
+//lets check how many items are in the array an also log them all ouy
+console.log(favoritesWebsites.length);
+console.log(favoritesWebsites);
+
+
+//lets remove one item from the array using .pop. 
+
+favoritesWebsites.pop()
+
+
+
+console.log(favoritesWebsites.length);
+console.log(favoritesWebsites);
+
