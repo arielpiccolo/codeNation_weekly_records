24f6b85a94a9f228521b3a1f33b283ca506cd623
arielpiccolo <arielpiccolo@gmail.com>
2020-05-21 11:08:38 +0100

    challenge8day3Finished

--- classes/week1/day3/challenge8day3.js
+++ classes/week1/day3/challenge8day3.js
@@ -1,9 +1,11 @@
+//create variables that will store 2 random numbers
 let num1 = (Math.floor(Math.random() * 10));
 let num2 = (Math.floor(Math.random() * 10));
 let sum = num1 + num2;
 
+//check for either even or odd number
 if (sum % 2 == 0) {
-    console.log(`${sum} is a even number`)
+    console.log(`${sum} is the sum of num1 + num2 and gives an even number`)
 } else {
-    (num1 * num2);
+    console.log('num1 * num2 gives' + ' ' + num1 * num2);
 }
