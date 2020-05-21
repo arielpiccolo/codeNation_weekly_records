64214ebb854f6b29f5a911c39dfc10ec7bd2177b
arielpiccolo <arielpiccolo@gmail.com>
2020-05-21 10:51:00 +0100

    challenge7day3Finished

---
+++ classes/week1/day3/challenge7day3.js
@@ -0,0 +1,23 @@
+const word  = "wqwerwqefwqedwedewdqwedqwedw";
+const word2 = "eqwerwqefwqedwedewdqwedqwedw";
+
+
+let fLetter = word.charAt(0);
+let lLetter = word.charAt(-0)
+
+
+if (fLetter == lLetter){
+  console.log("fist and last letters are the same");
+} else {
+    console.log("first and last letters are not the same")
+}  
+
+let fLetter2 = word2.charAt(0);
+let lLetter2 = word2.charAt(-0)
+
+
+if (fLetter2 == lLetter){
+  console.log("fist and last letters are the same");
+} else {
+    console.log("first and last letters are not the same")
+}  
