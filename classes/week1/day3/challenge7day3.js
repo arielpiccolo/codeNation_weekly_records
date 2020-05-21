84025de8df9226933e40810ff8c9bf0553024f3a
arielpiccolo <arielpiccolo@gmail.com>
2020-05-21 10:58:22 +0100

    challenge7day3Finished

--- classes/week1/day3/challenge7day3.js
+++ classes/week1/day3/challenge7day3.js
@@ -1,21 +1,23 @@
+// set string varialbles
 const word  = "wqwerwqefwqedwedewdqwedqwedw";
 const word2 = "eqwerwqefwqedwedewdqwedqwedw";
 
-
+//index for first and last character
 let fLetter = word.charAt(0);
 let lLetter = word.charAt(-0)
 
-
+//compare first and last letter and logout
 if (fLetter == lLetter){
   console.log("fist and last letters are the same");
 } else {
     console.log("first and last letters are not the same")
 }  
 
+//index for first and last character
 let fLetter2 = word2.charAt(0);
 let lLetter2 = word2.charAt(-0)
 
-
+//compare first and last letter and logout
 if (fLetter2 == lLetter){
   console.log("fist and last letters are the same");
 } else {
---
+++ classes/week1/day3/challenge8day3.js
@@ -0,0 +1,9 @@
+let num1 = (Math.floor(Math.random() * 10));
+let num2 = (Math.floor(Math.random() * 10));
+let sum = num1 + num2;
+
+if (sum % 2 == 0) {
+    console.log(`${sum} is a even number`)
+} else {
+    (num1 * num2);
+}
