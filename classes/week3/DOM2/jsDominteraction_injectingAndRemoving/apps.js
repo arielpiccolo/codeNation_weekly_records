70c74700307518b5dfa55045a36ce3332e4cf7e5
arielpiccolo <arielpiccolo@gmail.com>
2020-06-03 21:57:10 +0100

    udpate

--- classes/week3/DOM2/jsDominteraction_injectingAndRemoving/apps.js
+++ classes/week3/DOM2/jsDominteraction_injectingAndRemoving/apps.js
@@ -1,16 +1,17 @@
+//setting up variables
 const input = document.getElementById("input");
 const button = document.getElementById("submit");
-let list = document.getElementsByTagName("ul")[0];
-let showhidenbtn = document.getElementById("showhide-btn");
+
+let list = document.getElementsByClassName("ul")[0];
+
 
 
 button.addEventListener("click", ()=> {
- let listItem = document.createElement("li");
- listItem.textContent = input.value;
- list.appendChild(listItem);
- input.value = ('');
+    let listItem = document.createElement("li");
+    listItem.textContent = input.value;
+    
  //console.log(listItem);
 })
 
 
-
+console.log("testing page connnection");
