fe9fc04ec7e47a95cc5f1f7e8aa437be7e015786
arielpiccolo <arielpiccolo@gmail.com>
2020-06-15 15:20:06 +0100

    update

--- classes/week5/my_app/src/App.js
+++ classes/week5/my_app/src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './App.css';
 import Avatar from "./avatar.jpeg";
 
-class App extends React.Component{
+class App extends React.Component{ //! class component
   render(){
-    return(
-      <React.Fragment>
-        <div>
+    return(  //all the 'seudo-html' must be wrapped in a parent tag
+      <React.Fragment>  
+        
          <h1 className="title"> My first React app </h1>
          <img src={Avatar} alt="my img"/>
          <h1 id="important"> some text</h1>
-        </div>
+        
       </React.Fragment>
     );
   }
 }
 export default App;
+
+
+/**
+ *  babel will convert react code into native Javascript
+ * 
+ */
