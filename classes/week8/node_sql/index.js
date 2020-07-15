ad0d384891272678944425aafbeadc63edc58a69
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2020-07-15 17:43:26 +0100

    Add files via upload

--- classes/week8/node_sql/index.js
+++ classes/week8/node_sql/index.js
@@ -9,8 +9,8 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
-    
-    database: "employees_db"
+    port: 3306,
+    database: "classicmodels"
 });
 
 //setting up path sets and use
@@ -34,37 +34,83 @@ db.connect( (error) => {
 //main render to index.hbs
 app.get("/", (req, res) => {
 
-    // db.query('SELECT * FROM employees', (error, results) => {
-    //     console.log(results);
-   
+    db.query('SELECT * FROM users', (error, results) => {
+        console.log(results);
 
         res.render("index", {
-            // employees: results // [{}, {}]
+            users: results // [{}, {}]
         });     
     });
-// });
+});
+
+
+// this is hard coded now, but it for the sql project will need to be passed from a form at index.
+// ++++++++++++++++++++++++++++++++
+app.get("/register", (req, res) => {
+    let name = "Joe";
+    let age = 26;
+    let city = "York"
+// +++++++++++++++++++++++++++++++++
+
+
+    db.query('SELECT first_name FROM users WHERE first_name = ?',
+        [name], (error, results) => {
+            if( results.length > 0) {
+                res.render("register",{
+                    message: "Sorry that username has been taken"
+                })
+            } else {
+                db.query('INSERT INTO users SET ?',
+                {first_name: name, age: age, city: city}, (error, results) => {
+                    if(error) {
+                        res.render("register",{
+                            message: "Sorry there was an error"
+                        })
+                    } else {
+                        res.render("register",{
+                            message: "User registered"
+                        })
+                    }
+                });
+            }
+        })
+})
+
+
+
+// you always need an app.get before the app.post in order to be able to display
+app.get("/update", (req, res) => {
+    res.render("update");
+})
+// this is what coming next after the above app.get
+
+app.post("/update/:id", (req, res) => {
+    console.log(req.params.id);
+    let name  = req.body.user_name;
+    let id = req.params.id;
+    let sql = 'UPDATE users SET first_name = ? WHERE id = ?';
+    let user = [name, id];
+
+    db.query( sql, user, (error, results) => {
+        if(error) {
+            console.log(error);
+            res.render("update", {
+                message: "There was an error updating your user"
+            })
+        } else {
+            res.render("update", {
+                message: "User updated"
+            })
+        }
+    })
+
+
+
+    res.render("update");
+});
 
 
 //listening
-app.listen(3000, () => {
-    console.log("Server is running on Port 3000");
+app.listen(4000, () => {
+    console.log("Server is running on Port 4000");
 })
-
-//  app.get("register", (req, res) => {
-//     let name = "Helen";
-//     let age = 21;
-//     let city = "Toronto";
-
-//     db.query('insert into employees set ?', {first_name: name, age: age, city: city}, (error, results)) => {
-            // if(error) {
-                // res.render("register", {
-                //     message: "sorry there was an error"
-                // })
-            // { else }
-            //     res.render("register"), {
-            //         message: "registered"
-            //     }
-            // }
-// }
-
-// })
